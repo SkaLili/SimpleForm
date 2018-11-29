a76f7d87f5d09db9a4812a11364de496a2a8c8ee
Liliya <bulatovalilija@gmail.com>
2018-11-29 13:10:40 +0300

    Initial commit

---
+++ SimpleForm/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using SimpleForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using System.Xml.Linq;
+
+
+
+namespace SimpleForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Person p = new Person();
+            return View(p);
+        }
+        public ActionResult SetData(Person person)
+        {
+            string xmlFilePath = "~/Content/XMLFile.xml";//иногда приходиться указывать полный адрес
+            XDocument xDoc = XDocument.Load(xmlFilePath);
+            XElement xe = xDoc.Element("persons");
+            xe.Add( new XElement("person",
+                new XAttribute("name",person.Name),
+                new XElement("hotman",person.HotMen),
+                new XElement("talkative",person.Talkative)));
+            xDoc.Save(xmlFilePath);
+            person.HotMen = true;
+            return View(person);
+        }
+    }
+}
---
+++ SimpleForm/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleForm.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public bool HotMen { get; set; }
+        public int Talkative { get; set; }
+        
+    }
+}
